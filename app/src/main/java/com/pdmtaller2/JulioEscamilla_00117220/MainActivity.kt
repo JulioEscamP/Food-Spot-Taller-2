8125c52b69c251c3fef20ef3b14481b06589e5ec
JulioEscamP <00117220@uca.edu.sv>
2025-04-30 21:48:06 -0600

    Adding: structure

---
+++ app/src/main/java/com/pdmtaller2/JulioEscamilla_00117220/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.pdmtaller2.JulioEscamilla_00117220
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pdmtaller2.JulioEscamilla_00117220.ui.theme.FoodSpotByJulioEscamillaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FoodSpotByJulioEscamillaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FoodSpotByJulioEscamillaTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/pdmtaller2/JulioEscamilla_00117220/data/Models.kt
@@ -0,0 +1,2 @@
+package com.pdmtaller2.JulioEscamilla_00117220.data
+
